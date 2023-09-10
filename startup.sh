229b420a8d1415133e7844657840e7347d2d4591
Tyler R. Bundy <me@tylerbundy.me>
2023-09-10 12:14:08 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/with-contenv /bin/bash
 echo Chemotherapy: Installing SQLite3...
-apk add -U --no-cache sqlite
+
+# escalators escalators escalators escalators...
+if (type -p apk > /dev/null); then apk add -U --no-cache sqlite; fi
+if (type -p apt > /dev/null); then apt install sqlite -y; fi
+
+# eeeeeeeeels. well shit.
+if (type -p sqlite > /dev/null); then echo Chemotherapy: Not sure what distro we are on... help!; fi
 
 PUID="${PUID:-$(id -u)}"
 PGID="${PGID:-$(id -g)}"
@@ -21,7 +27,7 @@ for I in 1 2 3 4 5; do
       echo Chemotherapy: Checking for $dbpath/$dbname.${dbext}_remote: DOES NOT EXIST! 
       echo STOP!!! You need to rename the database file to $dbname\_remote otherwise your database will actually get cancer!
       echo A symlink will be created with the original filename - the symlink is ephemeral and WILL be removed when this script runs.
-      echo Let this be a lesson - back up your shit!
+      echo I almost deleted your database file, but this handy dandy check prevented me from doing so. Let this be a lesson - back up your shit!
       sleep infinity
     fi
 
