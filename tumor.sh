30d63a150168596c4f8c94ae873748d5f6f7160c
Tyler R. Bundy <me@tylerbundy.me>
2023-09-10 09:38:49 -0500

    Update tumor.sh

--- tumor.sh
+++ tumor.sh
@@ -1,37 +1,29 @@
-#!/bin/bash
+#!/usr/bin/with-contenv /bin/bash
 echo Chemotherapy: The synchronization tumor is lurking...
 
-$CHEMO_ENTRYPOINT
 CHEMO_CADENCE="${CHEMO_CADENCE:-900}"
 
 write_database() {
   echo Chemotherapy: Time for your appointment! Writing databases to NFS share...
   
   for I in 1 2 3 4 5; do
-    if [ ! -z "$CHEMO_DB$I" ]; then
-      dbuniq="CHEMO_DB$I"
-      dbfull=$CHEMO_DB$I
+    dbuniq='CHEMO_DB'$I
+    dbfull=${!dbuniq}
+
+    if [ ! -z "$dbfull" ]; then
       dbpath=$(dirname -- "$dbfull")
       dbfile=$(basename -- "$dbfull")
       dbname="${dbfile%.*}"
       dbext="${dbfile##*.}"
       
-      echo /tmp/$dbuniq.db_local -> $dbpath/$dbname.${dbext}_remote
+      echo Chemotherapy: Copying database /tmp/$dbuniq.db_local to $dbpath/$dbname.${dbext}_remote
       sqlite3 "/tmp/$dbuniq.db_local" ".backup $dbpath/$dbname.${dbext}_remote"
     fi
   done
 }
 
 while [ true ]; do
+  sleep $CHEMO_CADENCE
   write_database
   echo Chemotherapy: Synchronization appointment completed, waiting $CHEMO_CADENCE seconds...
-  sleep $CHEMO_CADENCE
 done
-
-chemo_death() {
-  echo Chemotherapy: Death is imminent. Preparing to rest in peace...
-  write_database
-  echo Chemotherapy: Databases have been written to the NFS share, goodbye!
-}
-
-trap chemo_death SIGINT SIGTERM SIGHUP SIGKILL
