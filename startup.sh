efebe85dc23bef4c5e4237ffe93def97c230f37c
Tyler R. Bundy <me@tylerbundy.me>
2023-09-10 07:29:55 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo Chemotherapy: Installing sqlite...
+echo Chemotherapy: Installing SQLite3...
 apk add -U --no-cache sqlite
+
+PUID="${PUID:-$(id -u)}"
+PGID="${PGID:-$(id -g)}"
+POSIX="${POSIX:-644}"
 
 for I in 1 2 3 4 5; do
   dbuniq="CHEMO_DB$I"
@@ -14,7 +18,7 @@ for I in 1 2 3 4 5; do
     dbext="${dbfile##*.}"
 
     if [ ! -f "$dbpath/$dbname.${dbext}_remote" ]; then
-      echo Chemotherapy: STOP!!! You need to rename this file to $dbname\_remote otherwise your database will actually get cancer!!!
+      echo Chemotherapy: STOP!!! You need to rename this file to $dbname\_remote otherwise your database will actually get cancer!
       echo A symlink will be created with the original filename - the symlink is ephemeral and WILL be removed when this script runs.
       echo Let this be a lesson - back up your shit!
       sleep infinity
@@ -25,6 +29,6 @@ for I in 1 2 3 4 5; do
     ln -sv /tmp/$dbuniq.db_local $dbpath/$dbname.${dbext}  
 
     chown $PUID:$PGID $dbpath/$dbname.${dbext}
-    chmod 644 $dbpath/$dbname.${dbext}
+    chmod $POSIX $dbpath/$dbname.${dbext}
   fi
 done
